3aa956f679da480f0521bab5ab9c082ea6413065
Parichaya <Pchhantyal001@my.wilmu.edu>
2025-12-27 14:07:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value;
 
+  if (taskText == "") {
+    alert("Enter a task");
+    return;
+  }
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.onclick = function () {
+    li.remove();
+  };
+  li.appendChild(deleteButton);
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
